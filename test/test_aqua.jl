2c33621de46d342a936becc3bc85eb7adbd4cefc
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2025-09-22 21:18:03 +0100

    Remove NowcastAutoGP from test dependencies
    NowcastAutoGP has been removed from the test/Project.toml dependencies. The Aqua test in test_aqua.jl is updated to call Aqua.test_all(NowcastAutoGP) without exclusions, simplifying the test configuration.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,14 +1,6 @@
 @testitem "Aqua Tests" begin
     using Aqua
-    using NowcastAutoGP
 
-    # Run all Aqua tests with appropriate exclusions
-    Aqua.test_all(
-        NowcastAutoGP;
-        ambiguities=false,      # Often has false positives
-        unbound_args=false,     # Can have issues with advanced macro usage
-        stale_deps=false,       # Not critical for initial setup
-        deps_compat=false,      # Not critical for initial setup
-        piracies=false,         # May have intentional type piracy
-    )
+    # Run all Aqua tests
+    Aqua.test_all(NowcastAutoGP)
 end
