04393d4ab52ea5df4fd787e8c77f2b524098033b
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2025-09-22 15:08:37 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,13 +9,15 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true",
         canonical = "https://cdcgov.github.io/NowcastAutoGP/",
         assets = ["assets/material-theme.css", "assets/material-theme.js"],
+        mathengine = Documenter.MathJax3()
     ),
     pages = [
         "Home" => "index.md",
+        "Getting started" => "vignettes/tutorial.md",
         "API Reference" => "api.md",
     ],
     # repo = "https://github.com/CDCgov/NowcastAutoGP/",
-    clean = true,
+    clean = false,
     checkdocs = :exports,
     remotes = nothing
 )
